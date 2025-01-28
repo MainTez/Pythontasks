2e868fa1f375d3216a7355ad19cd2824898700e7
maintez <danilebnen@gmail.com>
2025-01-28 10:47:10 +0100

    tasks

---
+++ Oppgave2.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter number here"))
+num2 = float(input("Enter number here"))
+
+result = num1+num2
+
+print(f"{result}")
---
+++ Oppgave21.py
@@ -0,0 +1,9 @@
+name = input("Please type in your full name")
+print(f"Welcome {name}!")
+
+
+# Ask the user for their full name
+full_name = input("Please type your full name: ")
+
+# Print the welcome message
+print(f"Welcome, {full_name}!")
